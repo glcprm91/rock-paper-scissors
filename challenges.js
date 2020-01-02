c65cc6b5f3f91f48be4268b6da4aeb0f39f1f550
Gianluca <gianluca8491@outlook.it>
2020-01-03 00:31:09 +0100

    logic complete
    1.1 -> implement graphic improvements now

--- challenges.js
+++ challenges.js
@@ -1,13 +1,12 @@
-// prettier-ignore-start
-let gamePlaying, game;
+let playerScore, comScore, gamePlaying, game;
 
 let choices = ["scissors", "rock", "paper"];
 
 init();
 
-document.querySelector(".scissors").addEventListener("click", startGame);
-document.querySelector(".rock").addEventListener("click", startGame);
-document.querySelector(".paper").addEventListener("click", startGame);
+document
+  .querySelectorAll(".button-choose")
+  .forEach(e => e.addEventListener("click", startGame));
 
 function startGame(e) {
   let playerChoose = String(...e.target.classList);
@@ -31,28 +30,69 @@ function startGame(e) {
     image.src = comHand + ".png";
 
     // 5. Update results
-
     let playerGame = document.querySelector(".player-game-" + game);
     let comGame = document.querySelector(".com-game-" + game);
     playerGame.classList.add("active");
     comGame.classList.add("active");
 
     if (playerChoose === "scissors" && comHand === "paper") {
+      playerScore++;
       updateResult(player, com); //PLAYER WON
+      checkGameWinner(playerScore, comScore);
     } else if (playerChoose === "scissors" && comHand === "rock") {
+      comScore++;
       updateResult(com, player); //COM WON
+      checkGameWinner(playerScore, comScore);
     } else if (playerChoose === "rock" && comHand === "paper") {
+      comScore++;
       updateResult(com, player); //COM WON
+      checkGameWinner(playerScore, comScore);
     } else if (playerChoose === "rock" && comHand === "scissors") {
+      playerScore++;
       updateResult(player, com); //PLAYER WON
+      checkGameWinner(playerScore, comScore);
     } else if (playerChoose === "paper" && comHand === "rock") {
+      playerScore++;
       updateResult(player, com); //PLAYER WON
+      checkGameWinner(playerScore, comScore);
     } else if (playerChoose === "paper" && comHand === "scissors") {
+      playerScore++;
       updateResult(com, player); //PLAYER WON
+      checkGameWinner(playerScore, comScore);
     } else {
       //DRAW
       player.classList.add("ion-android-remove");
       com.classList.add("ion-android-remove");
+      checkGameWinner(playerScore, comScore);
+    }
+  }
+}
+
+function checkGameWinner(playerScore, comScore) {
+  let playerPanel = document.querySelector(".player-0-panel");
+  let comPanel = document.querySelector(".player-1-panel");
+
+  if (playerScore === 3) {
+    playerPanel.classList.add("winner");
+    document.querySelector("#name-0").textContent = "Winner!";
+    gamePlaying = false;
+  } else if (comScore === 3) {
+    comPanel.classList.add("winner");
+    document.querySelector("#name-1").textContent = "Winner!";
+    gamePlaying = false;
+  } else if (game === 5) {
+    if (playerScore > comScore) {
+      playerPanel.classList.add("winner");
+      document.querySelector("#name-0").textContent = "Winner!";
+      gamePlaying = false;
+    } else if (playerScore < comScore) {
+      comPanel.classList.add("winner");
+      document.querySelector("#name-1").textContent = "Winner!";
+      gamePlaying = false;
+    } else if (playerScore === comScore) {
+      document.querySelector("#name-1").textContent = "Draw!";
+      document.querySelector("#name-0").textContent = "Draw!";
+      gamePlaying = false;
     }
   }
 }
@@ -60,6 +100,8 @@ function startGame(e) {
 document.querySelector(".btn-new").addEventListener("click", init);
 
 function init() {
+  playerScore = 0;
+  comScore = 0;
   game = 0;
   gamePlaying = true;
 
@@ -67,6 +109,9 @@ function init() {
   document.getElementById("name-1").textContent = "COM"; //RESET FOR WINNER TEXT
   document.querySelector(".player-hand").src = "rock.png"; //DISPLAY ROCK AS DEFAULT
   document.querySelector(".com-hand").src = "rock.png"; //DISPLAY ROCK AS DEFAULT
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
 
   //RESET SCORES
   for (let i = 1; i <= 5; i++) {
